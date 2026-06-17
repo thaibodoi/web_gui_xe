c0832b5dae22f9028ac8ac237473d9770059b879
dangngocson1756 <dangngocson1756@gmail.com>
2026-06-16 21:50:03 -0700

    Update ParkingCardRepository.java

--- parking-management-backend/src/main/java/com/group1/parking_management/repository/ParkingCardRepository.java
+++ parking-management-backend/src/main/java/com/group1/parking_management/repository/ParkingCardRepository.java
@@ -9,4 +9,7 @@ import com.group1.parking_management.entity.ParkingCard;
 public interface ParkingCardRepository extends JpaRepository<ParkingCard, Integer> {
     @org.springframework.data.jpa.repository.Query("SELECT COALESCE(MAX(c.cardId), 0) FROM ParkingCard c")
     Integer findMaxCardId();
+
+    // Xóa toàn bộ thẻ có cardId > giá trị truyền vào (dùng khi giảm giới hạn thẻ)
+    void deleteByCardIdGreaterThan(Integer cardId);
 }
